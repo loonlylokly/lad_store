bdf5600a2ed01f3fab771f88faf165b89854fedb
loonlylokly <loonlylokly@gmail.com>
2023-10-16 13:31:17 +0300

    fix: fix tag Link again but for categories

--- src/modules/Categories/Categories.tsx
+++ src/modules/Categories/Categories.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import Link from "next/link";
 import { filterSlice } from "@/store/reducers/FilterSlice";
 import { useAppDispatch } from "@/store/store";
 import styles from "./Categories.module.css";
@@ -13,15 +12,15 @@ export default function Categories() {
     <section>
       <h2 className={styles.title}>По категориям</h2>
       <div className={styles.categories}>
-        <Link className={styles.clothes} href="/catalog" onClick={() => dispatch(changeCategory('clothes'))}>
+        <a className={styles.clothes} href="/catalog" onClick={() => dispatch(changeCategory('clothes'))}>
           <p className={`${styles.name} btn`}>Одежда</p>
-        </Link>
-        <Link className={styles.dishes} href="/catalog" onClick={() => dispatch(changeCategory('dishes'))}>
+        </a>
+        <a className={styles.dishes} href="/catalog" onClick={() => dispatch(changeCategory('dishes'))}>
           <p className={`${styles.name} btn`}>Посуда</p>
-        </Link>
-        <Link className={styles.souvenirs} href="/catalog" onClick={() => dispatch(changeCategory('souvenirs'))}>
+        </a>
+        <a className={styles.souvenirs} href="/catalog" onClick={() => dispatch(changeCategory('souvenirs'))}>
           <p className={`${styles.name} btn`}>Сувениры</p>
-        </Link>
+        </a>
       </div>
     </section>
   )
